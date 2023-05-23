cd09623867cefd00e588c4d7a450336c0b86b1fe
cabemaiwaie <cabemaiwaie@stu.otc.school.nz>
2023-05-23 12:23:51 +1200

    started the sign up page

---
+++ main.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+from tkinter import *
+
+# main
+
+root = tk.Tk()
+root.title('Flashcard Sign Up')
+root.geometry('350x450')
+
+welcome_label = tk.Label(root, text="Welcome to My Flashcards")
+welcome_label.pack()
+
+sign_up_button = tk.Button(root, text="Sign Up", command=sign_up)
+sign_up_button.pack()
+
+
+# sign up function
+
+def sign_up():
+    sign_up
+
+
+root.mainloop()
